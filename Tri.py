cfbd0cb6af3ae97d73cf260ede15ff4b144abc4d
SumuduDeshani <91952053+SumuduDeshani@users.noreply.github.com>
2021-10-08 14:16:20 +0530

    UpdateTri.py
    Lower part of the code

--- Tri.py
+++ Tri.py
@@ -1,3 +1,6 @@
 a=float(input('Enter the first side:'))
 b=float(input('Enter the second side:'))
 c=float(input('Enter the Third side:'))
+s=(a+b+c)/2
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print('Area of the triangle is %0.2f %area)
